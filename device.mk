790d02ef84b041048bc5086efb461802a2e34d7b
Bowgo Tsai <bowgotsai@google.com>
2019-12-16 11:51:20 +0800

    zahedan: Adding developer GSI keys
    Bug: 144399552
    Test: tree-hugger
    Change-Id: I5012758b4f25bd0d14bca906358ecee0fa366a7b

--- device.mk
+++ device.mk
@@ -14,6 +14,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
