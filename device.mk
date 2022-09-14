a59183afc43f019d5653f7930b67d8281a314fee
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    zahedan: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -124,7 +124,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # Display
 PRODUCT_PACKAGES += \
