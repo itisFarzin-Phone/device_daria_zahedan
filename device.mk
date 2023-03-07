9b3cbc6fc0c44b503d46d60fdcd1d700a301569f
Erfan Abdi <erfangplus@gmail.com>
2023-03-07 15:40:16 +0330

    zahedan: Import recovery init script
    Change-Id: If412f0956b7a699549563d2b7e4a857afe46101f

--- device.mk
+++ device.mk
@@ -310,6 +310,7 @@ PRODUCT_PACKAGES += \
     init.mt6877.rc \
     init.mt6877.usb.rc \
     init.project.rc \
+    init.recovery.mt6877.rc \
     init.sensor_2_0.rc \
     init.stnfc.rc \
     ueventd.mtk.rc
