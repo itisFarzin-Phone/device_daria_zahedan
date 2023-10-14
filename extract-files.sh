4c7cf36390cf24b864dd3f1958bba3aea23447f2
Tim Zimmermann <tim@linux4.de>
2023-10-14 21:20:47 +0330

    zahedan: Patch widevine to load libprotobuf-cpp-full-3.9.1
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -69,6 +69,12 @@ function blob_fixup {
         vendor/lib64/libmtkcam_stdutils.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
+        vendor/lib64/libwvhidl.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
+        vendor/lib64/mediadrm/libwvdrmengine.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
     esac
 }
 
