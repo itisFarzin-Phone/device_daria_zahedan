516e3a91b846d78ffff65182126b0d5594f4d96e
electimon <electimon@gmail.com>
2024-01-29 01:12:23 +0000

    zahedan: Build sensors.zahedan
    Change-Id: I7d67dbd426f97dd64332bd0e7bd2884028926fde
    Signed-off-by: electimon <electimon@gmail.com>

--- device.mk
+++ device.mk
@@ -367,8 +367,8 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1.vendor \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
     android.frameworks.sensorservice@1.0.vendor \
-    sensors.udfps \
-    libsensorndkbridge
+    libsensorndkbridge \
+    sensors.zahedan
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
